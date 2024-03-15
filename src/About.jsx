d8995772adad553ebff886f08d82722655fc17f2
kabore r evariste <kaboreva@gmail.com>
2024-03-15 10:23:04 +0100

    modification du bloc à propos

--- src/About.jsx
+++ src/About.jsx
@@ -13,13 +13,17 @@ function About() {
           <p className="text-slate-600 mt-8 leading-loose">
             Je suis ingénieur de formation et un passionné de code. dans le
             cadre de mon travail, je peux prendre en charge le développement du
-            backend et/ou du frontend d'une application.j'ai un profil avec une
-            forte expérience des technologies JavaScript.
+            backend et/ou du frontend d'une application sans oublier la partie
+            conception evidemment.
           </p>
           <p className="text-slate-600 mt-8 leading-loose">
-            je suis titulaire de deux diplomes d'ingénieur, un à EMSI( Ecole
-            Marocaine des Sciences de l'ingénieur) et l'autre est de
-            l'université Sophia Antipolis de Nice
+            J'ai déjà eu l'opportunité de travailler sur plusieurs projets
+            utilisant divers technologies et langages de programmation.
+          </p>
+          <p>
+            J'ai un bon niveau d'analyse et de résolution de problèmes et j'aime
+            également travailler sur des projets innovants qui apportent de la
+            plus value aux utilisateurs finaux de nos projets.
           </p>
         </article>
       </div>
